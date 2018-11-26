27fad3eb860da9c687d7c4c713ecd7b6d3b39ba4
Karen Pacheco <pachecovejar.k@gmail.com>
2018-11-26 17:43:09 -0300

    add style

--- App.js
+++ App.js
@@ -1,38 +1,26 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
-
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isShowingText: true };
-
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
-  }
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+        <Text style={styles.red}>Red</Text>
+      <Text style={styles.bigblue}>Bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
