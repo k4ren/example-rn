48f28eebe1e0e532260b739d6f9d89df1bdabb56
Karen Pacheco <pachecovejar.k@gmail.com>
2018-11-26 17:58:17 -0300

    fixed dimensions

--- App.js
+++ App.js
@@ -5,22 +5,10 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.red}>Red</Text>
-      <Text style={styles.bigblue}>Bigblue</Text>
-        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
