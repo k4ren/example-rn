ff623b9fcc756a532e4a83d9c6ab1110417f931a
Karen Pacheco <pachecovejar.k@gmail.com>
2018-11-26 18:15:28 -0300

    justify content

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ export default class App extends Component {
     return (
       <View style={{
           flex: 1,
-          flexDirection: 'row'}}>
+          flexDirection: 'column',
+          justifyContent: 'space-between'
+        }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
